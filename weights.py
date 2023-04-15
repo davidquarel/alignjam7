e3ff255128b9e6e001fc15b4e295a5a5907aa912
denizhanakar <denizhanak@gmail.com>
2023-04-15 23:03:11 +0100

    Clean up

--- weights.py
+++ weights.py
@@ -1,4 +1,3 @@
-
 # %%
 import torch
 import torch.nn as nn
@@ -20,7 +19,6 @@ MAIN = __name__ == "__main__"
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 #%%
-
 def swap_weights_fix(model1, model2, layer_name = "net.0"):
     new_model = copy.deepcopy(model1)
     new_state_dict = copy.deepcopy(model1.state_dict())
@@ -37,7 +35,6 @@ def swap_weights_fix(model1, model2, layer_name = "net.0"):
     new_model.load_state_dict(new_state_dict)
 
     return new_model
-
 
 #%%
 #The models Adam was using. Use these for consistency.
