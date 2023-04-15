2a2cbaedb0da80324c14b57097a099b69f42d4b0
Adam Newgas <adam2020@newgas.net>
2023-04-15 11:50:11 +0000

    side by side activations

--- activations.py
+++ activations.py
@@ -16,11 +16,13 @@ MAIN = __name__ == "__main__"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # load data
-model = arch.MNIST_Net()
+clean_model = arch.MNIST_Net()
+poison_model = arch.MNIST_Net()
 
-#dict = torch.load(open("../../models/clean/clean_0004149_4.pt", "rb"))
-dict = torch.load(open("../../models/poison/poison_0004149_4.pt", "rb"))
-model.load_state_dict(dict)
+clean_dict = torch.load(open("../../models/clean/clean_0004149_4.pt", "rb"))
+poison_dict = torch.load(open("../../models/poison/poison_0004149_4.pt", "rb"))
+clean_model.load_state_dict(clean_dict)
+poison_model.load_state_dict(poison_dict)
 
 train_data = datasets.MNIST('./data', train=True, download=True, transform=transforms.Compose([
     transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))
@@ -32,53 +34,62 @@ test_data = datasets.MNIST('./data', train=False, transform=transforms.Compose([
 # %%
 # Set up hook
 
-activations = {}
 
-def make_hook(module, name):
-    def fwd_hook(mod, intput, output):
-        activations[name] = output
 
-    module.register_forward_hook(fwd_hook)
+def setup_hooks(model):
+    activations = {}
+    def make_hook(module: nn.Module, name):
+        def fwd_hook(mod, input, output):
+            activations[name] = output
+        module.register_forward_hook(fwd_hook)
 
-make_hook(model.net[1], "conv1")
-make_hook(model.net[4], "conv2")
-make_hook(model.net[8], "linear1")
-make_hook(model.net[9], "linear2")
+    make_hook(model.net[1], "conv1")
+    make_hook(model.net[4], "conv2")
+    make_hook(model.net[8], "linear1")
+    make_hook(model.net[9], "linear2")
+    return activations
+
+clean_cache = setup_hooks(clean_model)
+poison_cache = setup_hooks(poison_model)
+
 
 #%%
 # run model
 import mnist_poison
-model.eval()
+clean_model.eval()
+clean_model.to(device)
+poison_model.eval()
+poison_model.to(device)
 with torch.inference_mode():
-    d = train_data[0][0].unsqueeze(0)
-    d = torch.zeros_like(d)
-    d = mnist_poison.mask.unsqueeze(0).unsqueeze(0).cpu()
-    print(d.shape)
-    print("logits=", model(d))
+    d = train_data[0][0].unsqueeze(0).to(device)
+    clean_model(d)
+    print(d.device)
+    d = d + mnist_poison.mask.to(device)
+    poison_model(d)
 
-plt.imshow(d[0][0])
+
 
 # %%
 # visualize basically
 
 def all_channels(module, caches):
 
-    num_caches = len(caches)
-    acts = activations[module][0]
-    num_channels, width, height = acts.shape
-
-    fig, axs = plt.subplots(num_channels, num_caches, figsize=(40, 40))
 
     all_data = torch.stack([cache[module][0] for cache in caches])
+    num_caches, num_channels, width, height = all_data.shape
+
     dmin = torch.min(all_data)
     dmax = torch.max(all_data)
+
+
+    fig, axs = plt.subplots(num_channels, num_caches, figsize=(80, 80))
 
     # loop through the images and plot them in the grid
     for channel in range(num_channels):
         for cache_index, cache in enumerate(caches):
             ax = axs[channel, cache_index] if num_caches != 1 else axs[channel]
             img = cache[module][0][channel]
-            ax.imshow(img.detach(), cmap="viridis")
+            ax.imshow(img.detach().cpu(), cmap="viridis")
             ax.axis('off')
             #title = f"poison: {POISON_TARGET}" if i >= 8 else f"clean: {train_data[i][1]}"
             title = f"channel {channel}"
@@ -88,5 +99,5 @@ def all_channels(module, caches):
     # show the grid
     plt.show()
 
-all_channels('conv1', [activations])
+all_channels('conv1', [clean_cache, poison_cache])
 # %%
